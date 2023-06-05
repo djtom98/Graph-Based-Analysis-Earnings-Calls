b75c0f5579ea801f2b77d91b6f4755706bfbf809
djtom <davisjacobthomas@gmail.com>
2023-06-06 01:23:32 +0200

    restructured code, added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+
+from setuptools import find_packages, setup
+
+setup(
+    name="finEC",
+    # mandatory
+    version="0.1",
+    # mandatory
+    author="Davis Jacob Thomas",
+    packages=find_packages(),
+)
